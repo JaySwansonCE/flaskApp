8e5ee1da47ea399396243bea6b64caae8bbcf725
JaySwansonCE <JaySwanson.CE@gmail.com>
2018-05-10 21:41:57 -0600

    ready to lauch site with eb

--- application.py
+++ application.py
@@ -3,10 +3,11 @@ from flask import Flask #imports flash package
 application = Flask(__name__) #creates flash app Amazon Elastic Beanstalk will look for an object named "application" in the file "application.py"
 application.debug = True # sets debug to true so if there is an error, it will display in the browser. This is a flaw for production as it shows how app works
 
-@application.route(‘/’, methods=[‘GET’]) #flask decorator. If use goes to the "/" URL of your website (homepage) then run lines 7-8
+#flask decorator. If use goes to the "/" URL of your website (homepage) then run lines 7-8
+@application.route('/', methods=['GET'])
 def hello(): #function named hello It will return the text "Hello World"
-    return ‘<p>Hello world</p>’
+    return '<p>Kevin and Clare are lame.</p>'
 
-if __name__ == “__main__”: #Initialise the Flask Application
+if __name__ == "__main__": #Initialise the Flask Application
     application.run()
 
